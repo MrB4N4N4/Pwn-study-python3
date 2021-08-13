5a87ff99bf0b9491b024b11e2cf294beac2d6aaa
MrB4N4N4 <2016rudgns@naver.com>
2021-08-13 04:54:40 -0400

    echo2-remote test

--- pwnable-kr/test.py
+++ pwnable-kr/test.py
@@ -7,3 +7,11 @@ Exploit echo2 succeed in local.
 Apply it remote environment!!
 First I need to test FSB-64bit using "%1$lx"
 """
+
+
+def select_menu(num):
+    print(p.recvuntil(b"> "))
+    r.sendline(num.encode())
+
+pay = "%"
+select_menu("2")
