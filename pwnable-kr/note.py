1faa803255b46d2884d30ac8b7aa16075fa3f508
MrB4N4N4 <2016rudgns@naver.com>
2021-08-26 04:05:10 -0400

    mmap is wierd

--- pwnable-kr/note.py
+++ pwnable-kr/note.py
@@ -1,11 +1,19 @@
 from pwn import *
 
 """
+0x31337 >> hacker menu
 [Protections]
 -NX, Partial RELRO
 [Bug]
-
+    mem_arr[256] - always empty
+    write_note.gets() - over flow
+    
 [Address]
 
 [Exploit]
 """
+
+p = process("/root/Downloads/note")
+#p = remote("pwnable.kr", 9019)
+
+
